bba9bbdebe490939fab2a11080415cfaf21b91d8
paul <paulvanall@charter.net>
2021-08-23 12:55:43 -0400

    systemd edits

--- step1.sh
+++ step1.sh
@@ -29,7 +29,10 @@ wget https://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-8.6p1.tar.gz
 wget https://www.sudo.ws/dist/sudo-1.9.7p1.tar.gz
 wget https://ftp.gnu.org/gnu/parted/parted-3.4.tar.xz
 
-mkdir -pv $LFS/{bin,etc,lib,sbin,usr,var}
+mkdir -pv $LFS/{etc,var} $LFS/usr/{bin,lib,sbin}
+for i in bin lib sbin; do
+  ln -sv usr/$i $LFS/$i
+done
 case $(uname -m) in
   x86_64) mkdir -pv $LFS/lib64 ;;
 esac
