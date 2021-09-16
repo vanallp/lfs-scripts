7eaec57a1e4c16f8f7b51a68db746a34f1d54dfc
paul <paulvanall@charter.net>
2021-09-16 12:04:15 -0400

    lxde

--- blfs-37LXDE.sh
+++ blfs-37LXDE.sh
@@ -481,6 +481,7 @@ ninja
 ninja install ;rc=$?;echo $package_name $rc >> /sources/37rc.log
 finish
 
+
 # https://ftp.gnu.org/gnu/which/which-2.21.tar.gz
 wget --no-check-certificate https://ftp.gnu.org/gnu/which/which-2.21.tar.gz
 begin which-2.21 tar.gz
@@ -488,6 +489,25 @@ begin which-2.21 tar.gz
 make
 make install;rc=$?;echo $package_name $rc >> /sources/37rc.log
 finish
+
+
+# https://github.com/silnrsi/graphite/releases/download/1.3.14/graphite2-1.3.14.tgz
+wget --no-check-certificate  https://github.com/silnrsi/graphite/releases/download/1.3.14/graphite2-1.3.14.tgz
+begin graphite2-1.3.14 tgz
+sed -i '/cmptest/d' tests/CMakeLists.txt
+mkdir build &&
+cd    build &&
+cmake -DCMAKE_INSTALL_PREFIX=/usr .. &&
+make
+make docs
+make install ;rc=$?;echo $package_name $rc >> /sources/37rc.log
+install -v -d -m755 /usr/share/doc/graphite2-1.3.14 &&
+cp      -v -f    doc/{GTF,manual}.html \
+                    /usr/share/doc/graphite2-1.3.14 &&
+cp      -v -f    doc/{GTF,manual}.pdf \
+                    /usr/share/doc/graphite2-1.3.14
+finish
+
 
 # https://github.com/harfbuzz/harfbuzz/releases/download/2.9.0/harfbuzz-2.9.0.tar.xz
 wget --no-check-certificate https://github.com/harfbuzz/harfbuzz/releases/download/2.9.0/harfbuzz-2.9.0.tar.xz
